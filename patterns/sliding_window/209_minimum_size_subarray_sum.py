aa3c526f69cb282d5f7308752c3ee2c4f33d81b6
Abir Hussain Khan Abid <abirkhanrcc@gmail.com>
2024-06-08 23:29:51 +0600

    feat: solve 209_minimum_size_subarray_sum

--- patterns/sliding_window/209_minimum_size_subarray_sum.py
+++ patterns/sliding_window/209_minimum_size_subarray_sum.py
@@ -1,1 +1,14 @@
-print("hello world")
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left, current_sum = 0, 0
+        min_length = float('inf')
+        
+        for right in range(len(nums)):
+            current_sum += nums[right]
+
+            while current_sum >= target:
+                min_length = min(min_length, right - left + 1)
+                current_sum -= nums[left]
+                left += 1
+
+        return 0 if min_length == float('inf') else min_length
