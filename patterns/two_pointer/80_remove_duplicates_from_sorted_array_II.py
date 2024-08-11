53ffe3f58ce1dec02e4316fa9d0e04fdc06129a1
Abir Hussain Khan Abid <abirkhanrcc@gmail.com>
2024-08-12 00:14:10 +0600

    feat: solve remove duplicates from sorted array II

---
+++ patterns/two_pointer/80_remove_duplicates_from_sorted_array_II.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        for j in range(len(nums)):
+            if i < 2 or nums[j] != nums[i-2]:
+                nums[i] = nums[j]
+                i+=1
+        return i
